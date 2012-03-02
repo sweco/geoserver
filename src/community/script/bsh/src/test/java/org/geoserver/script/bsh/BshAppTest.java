a7b34407db2b66208e9419458fa793e685c2d25c
jdeolive <jdeolive@gmail.com>
2012-03-01 18:01:14 -0700

    added a beanshell script module

---
+++ src/community/script/bsh/src/test/java/org/geoserver/script/bsh/BshAppTest.java
@@ -0,0 +1,36 @@
+package org.geoserver.script.bsh;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.geoserver.platform.GeoServerExtensions;
+import org.geoserver.script.ScriptManager;
+import org.geoserver.test.GeoServerTestSupport;
+
+import com.mockrunner.mock.web.MockHttpServletResponse;
+
+public class BshAppTest extends GeoServerTestSupport {
+
+    File app;
+    
+    @Override
+    protected void setUpInternal() throws Exception {
+        super.setUpInternal();
+    
+        app = getScriptManager().findOrCreateAppDir("foo");
+    }
+
+    protected ScriptManager getScriptManager() {
+        return GeoServerExtensions.bean(ScriptManager.class);
+    }
+
+    public void testSimple() throws Exception {
+        FileUtils.copyURLToFile(
+            getClass().getResource("index-helloWorld.bsh"), new File(app, "index.bsh"));
+
+        MockHttpServletResponse resp = getAsServletResponse("/apps/foo/index.bsh");
+        assertEquals(200, resp.getStatusCode());
+        assertEquals("Hello World!", resp.getOutputStreamContent());
+
+    }
+}
