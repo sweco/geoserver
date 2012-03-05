61a80ac0a3539e6f087621bd321c770e1aa42adf
jdeolive <jdeolive@gmail.com>
2012-03-05 14:01:21 -0700

    added editor for scripts with a tree widget, updated codemirror editor version

--- src/web/core/src/main/java/org/geoserver/web/wicket/CodeMirrorEditor.js
+++ src/web/core/src/main/java/org/geoserver/web/wicket/CodeMirrorEditor.js
@@ -1,60 +1,54 @@
 var textarea = document.getElementById('$componentId');
-var editor = CodeMirror.fromTextArea("$componentId", { 
-    height: "450px",
-    content: textarea.value,
-    tabMode: "shift",
-    parserfile: "$syntax",
-    lineNumbers: true, 
-    textWrapping: false,
-    disableSpellcheck: true,
-    stylesheet: "$stylesheet",
-    path: "./resources/org.geoserver.web.wicket.CodeMirrorEditor/js/codemirror/js/",
-    initCallback: function(ed) {
-       ed.win.document.body.style.fontSize = 12;
+var editor = CodeMirror.fromTextArea(textarea, { 
+    $options,
+    onBlur: function(e) {
+        if ($onBlurCallbackURL) {
+          wicketAjaxPost($onBlurCallbackURL, e.getValue());
+        }
     }
 });
 if(!document.gsEditors) {
-	document.gsEditors = {};
+    document.gsEditors = {};
 }
 document.gsEditors.$componentId = editor;
 document.getElementById('cm_undo').onclick = function() {
-	editor.undo();
+    editor.undo();
 };
 document.getElementById('cm_redo').onclick = function() {
-	editor.redo();
+    editor.redo();
 };
 document.getElementById('cm_goto').onclick = function() {
     var line = Number(prompt("Jump to line:", ""));
-    var lastLine = editor.lineNumber(editor.lastLine());
-    if (line && !isNaN(line)) {
-      if(line > lastLine) {
-    	  editor.jumpToLine(lastLine)
-      } else if(line < 1) {
-    	  editor.jumpToLine(1)
-      } else {
-    	  editor.jumpToLine(line);
-      }
+    var last = editor.lineCount();
+    line = (line <= 1) ? 1 : (line > last) ? last : line;
+    line = line - 1;
+    
+    if (!isNaN(line)) {
+      editor.setCursor(line,0);
+      editor.setSelection({line:line,ch:0},{line:line+1,ch:0});
+      editor.focus();
     }
 };
 document.getElementById('cm_font_size').onchange = function() {
-	var fontSize = document.getElementById('cm_font_size').value;
-	editor.lineNumbers.childNodes[0].style.fontSize = fontSize + "px";
-	editor.win.document.body.style.fontSize = fontSize;
+    var fontSize = document.getElementById('cm_font_size').value;
+    editor.getScrollerElement().style.fontSize = fontSize + "px";
 }
-document.getElementById('cm_reformat').onclick = function() {
-	if(editor.selection()) {
-		editor.reindentSelection();
-	} else {
-		editor.reindent();
-	}	
-}
+
+//JD: no longer really needed with smart indent
+/*document.getElementById('cm_reformat').onclick = function() {
+    if(editor.selection()) {
+        editor.reindentSelection();
+    } else {
+        editor.reindent();
+    }   
+}*/
 // This comes from http://thereisamoduleforthat.com/content/making-div-fullscreen-and-restoring-it-its-original-position
 // Does not work so commented out
 /*
 document.getElementById('cm_fullscreen').onclick = function() {
-	div = $('#$container');
+    div = $('#$container');
     if (!div.hasClass('fullscreen')) { // Going fullscreen:
-    	alert("Sigh, can't make this work at all...");
+        alert("Sigh, can't make this work at all...");
       // Save current values.
       editor.beforeFullscreen = {
         parentElement: div.parent(),
@@ -70,9 +64,9 @@ document.getElementById('cm_fullscreen').onclick = function() {
       // Restore saved values.
       div.removeClass('fullscreen');
       if (editor.beforeFullscreen.index >= editor.beforeFullscreen.parentElement.children().length) {
-    	  editor.beforeFullscreen.parentElement.append(div);
+          editor.beforeFullscreen.parentElement.append(div);
       } else {
-    	  div.insertBefore(editor.beforeFullScreen.parentElement.children().get(editor.beforeFullscreen.index));
+          div.insertBefore(editor.beforeFullScreen.parentElement.children().get(editor.beforeFullscreen.index));
       }
       $('body').css('overflow', 'auto');
       window.scroll(editor.beforeFullscreen.x, editor.beforeFullscreen.y);
