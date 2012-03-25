85d715541b4ea1f979b96fa2272be65b0f8a76e0
Tim Schaub <tim.schaub@gmail.com>
2012-03-24 22:48:00 -0600

    Separate JsgiRequest.

--- src/community/script/js/src/main/java/org/geoserver/script/js/JavaScriptAppHook.java
+++ src/community/script/js/src/main/java/org/geoserver/script/js/JavaScriptAppHook.java
@@ -2,24 +2,18 @@ package org.geoserver.script.js;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Logger;
 
 import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptException;
 
-import org.apache.commons.lang.StringUtils;
 import org.geoserver.script.ScriptPlugin;
 import org.geoserver.script.app.AppHook;
 import org.geoserver.script.js.engine.RhinoScriptEngine;
 import org.geotools.util.logging.Logging;
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.Scriptable;
-import org.mozilla.javascript.ScriptableObject;
-import org.restlet.data.Form;
-import org.restlet.data.Reference;
 import org.restlet.data.Request;
 import org.restlet.data.Response;
 
@@ -50,7 +44,7 @@ public class JavaScriptAppHook extends AppHook {
         Context cx = RhinoScriptEngine.enterContext();
         Scriptable jsgiRequest = null;
         try {
-            jsgiRequest = getJsgiRequest(request, response, cx);
+            jsgiRequest = new JsgiRequest(request, response, cx, jsPlugin.global);
         } catch (Exception e) {
             throw new ScriptException(e);
         } finally {
@@ -72,70 +66,4 @@ public class JavaScriptAppHook extends AppHook {
             throw new ScriptException("Failed to write response: " + e.getMessage());
         }
     }
-
-    /**
-     * Generates a JavaScript object that conforms to the JSGI spec.
-     * http://wiki.commonjs.org/wiki/JSGI/Level0/A/Draft2
-     * 
-     * @param request
-     * @param cx
-     * @return
-     */
-    Scriptable getJsgiRequest(Request request, Response response, Context cx) {
-
-        JavaScriptPlugin jsPlugin = (JavaScriptPlugin) plugin;
-        Scriptable jsgiRequest = cx.newObject(jsPlugin.global);
-        Reference ref = request.getResourceRef();
-
-        jsgiRequest.put("method", jsgiRequest, request.getMethod().toString());
-
-        jsgiRequest.put("scriptName", jsgiRequest, ref.getLastSegment());
-
-        List<String> seg = new ArrayList<String>(ref.getSegments().subList(4, ref.getSegments().size()));
-        seg.add(0, "");
-        jsgiRequest.put("pathInfo", jsgiRequest, StringUtils.join(seg.toArray(), "/"));
-        
-        jsgiRequest.put("queryString", jsgiRequest, ref.getQuery());
-        
-        jsgiRequest.put("host", jsgiRequest, ref.getHostDomain());
-        
-        jsgiRequest.put("port", jsgiRequest, ref.getHostPort());
-        
-        jsgiRequest.put("scheme", jsgiRequest, ref.getScheme());
-
-        try {
-            jsgiRequest.put("input", jsgiRequest, Context.javaToJS(request.getEntity().getStream(), jsPlugin.global));
-        } catch (IOException e) {
-            LOGGER.warning(e.getMessage());
-        }
-
-        Scriptable headers = cx.newObject(jsPlugin.global);
-        Form requestHeaders = (Form) request.getAttributes().get("org.restlet.http.headers");
-        for (String name : requestHeaders.getNames()) {
-            String value = requestHeaders.getFirstValue(name, true);
-            headers.put(name.toLowerCase(), headers, value);
-        }
-        jsgiRequest.put("headers", jsgiRequest, headers);
-
-        // create jsgi object
-        Scriptable jsgiObject = cx.newObject(jsPlugin.global);
-        int readonly = ScriptableObject.PERMANENT | ScriptableObject.READONLY;
-        Scriptable version = cx.newArray(jsPlugin.global, new Object[] {Integer.valueOf(0), Integer.valueOf(3)});
-        ScriptableObject.defineProperty(jsgiObject, "version", version, readonly);
-        ScriptableObject.defineProperty(jsgiObject, "multithread", Boolean.TRUE, readonly);
-        ScriptableObject.defineProperty(jsgiObject, "multiprocess", Boolean.FALSE, readonly);
-        ScriptableObject.defineProperty(jsgiObject, "async", Boolean.TRUE, readonly);
-        ScriptableObject.defineProperty(jsgiObject, "runOnce", Boolean.FALSE, readonly);
-        ScriptableObject.defineProperty(jsgiObject, "cgi", Boolean.FALSE, readonly);
-        jsgiRequest.put("jsgi", jsgiRequest, jsgiObject);
-
-        // create env object
-        Scriptable env = cx.newObject(jsPlugin.global);
-        env.put("servletRequest", env, Context.javaToJS(request, jsPlugin.global));
-        env.put("servletResponse", env, Context.javaToJS(response, jsPlugin.global));
-        jsgiRequest.put("env", jsgiRequest, env);
-        
-        return jsgiRequest;
-    }
-
 }
