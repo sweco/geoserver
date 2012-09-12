c320a17c65187c19024f47b2bd4a2b13c490c158
tschaub <tim.schaub@gmail.com>
2012-09-12 14:56:50 -0600

    Add script filename to runtime exceptions.

--- src/community/script/js/src/main/java/org/geoserver/script/js/JavaScriptWpsHook.java
+++ src/community/script/js/src/main/java/org/geoserver/script/js/JavaScriptWpsHook.java
@@ -23,10 +23,14 @@ public class JavaScriptWpsHook extends WpsHook {
 
     @Override
     public String getTitle(ScriptEngine engine) throws ScriptException {
+        String filename = (String) engine.get(ScriptEngine.FILENAME);
+        if (filename == null) {
+            filename = "<Unknown Source>";
+        }
         MetaProcess process = getProcess(engine);
         String title = process.getTitle();
         if (title == null) {
-            LOGGER.warning("Process missing required title.");
+            LOGGER.warning("Process missing required title in " + filename);
             // TODO provide process name
             title = "Untitled";
         }
@@ -55,19 +59,23 @@ public class JavaScriptWpsHook extends WpsHook {
     }
 
     private MetaProcess getProcess(ScriptEngine engine) {
+        String filename = (String) engine.get(ScriptEngine.FILENAME);
+        if (filename == null) {
+            filename = "<Unknown Source>";
+        }
         Object exportsObj = engine.get("exports");
         Scriptable exports = null;
         if (exportsObj instanceof Scriptable) {
             exports = (Scriptable) exportsObj;
         } else {
-            throw new RuntimeException("Couldn't get exports for process.");
+            throw new RuntimeException("Couldn't get exports for process in " + filename);
         }
         Object processObj = exports.get("process", exports);
         MetaProcess process = null;
         if (processObj instanceof Wrapper) {
             process = (MetaProcess) ((Wrapper) processObj).unwrap();
         } else {
-            throw new RuntimeException("Missing 'process' export.");
+            throw new RuntimeException("Missing 'process' exports from " + filename);
         }
         return process;
     }
