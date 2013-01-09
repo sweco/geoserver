9ed9d685f9167c68cd407c605fd10695288ebd59
Juan Marin <juan.marin.otero@gmail.com>
2013-01-09 14:45:03 -0500

    Add initial files

---
+++ src/extension/control-flow/src/main/java/org/geoserver/flow/rest/FlowConfigFinder.java
@@ -0,0 +1,24 @@
+/* Copyright (c) 2001 - 2012 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
+ * application directory.
+ */
+
+package org.geoserver.flow.rest;
+
+import org.restlet.Finder;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+import org.restlet.resource.Resource;
+
+/**
+ * 
+ * @author Juan Marin, OpenGeo
+ * 
+ */
+public class FlowConfigFinder extends Finder {
+
+    @Override
+    public Resource findTarget(final Request request, Response response) {
+        return null;
+    }
+}
---
+++ src/extension/control-flow/src/main/java/org/geoserver/flow/rest/FlowConfigResource.java
@@ -0,0 +1,25 @@
+/* Copyright (c) 2001 - 2012 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
+ * application directory.
+ */
+
+package org.geoserver.flow.rest;
+
+import org.geoserver.rest.ReflectiveResource;
+
+/**
+ * 
+ * @author Juan Marin, OpenGeo
+ * 
+ */
+public class FlowConfigResource extends ReflectiveResource {
+
+    public FlowConfigResource() {}
+    
+    @Override
+    protected Object handleObjectGet() throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
