2cd2a22e79fe9c1d0dca64494b4ad04d6cb46bcb
Andrea Aime <andrea.aime@gmail.com>
2015-06-22 16:20:05 +0200

    [GEOS-7084] Add a way to execute a import from REST in a single call

--- src/extension/importer/core/src/main/java/org/geoserver/importer/Importer.java
+++ src/extension/importer/core/src/main/java/org/geoserver/importer/Importer.java
@@ -332,7 +332,7 @@ public class Importer implements DisposableBean, ApplicationListener {
      * @param prepData
      * @return
      */
-    public Long initAsynch(final ImportContext context, final boolean prepData) {
+    public Long initAsync(final ImportContext context, final boolean prepData) {
         return jobs.submit(new Job<ImportContext>() {
             @Override
             protected ImportContext call(ProgressMonitor monitor) throws Exception {
@@ -834,9 +834,16 @@ public class Importer implements DisposableBean, ApplicationListener {
     }
 
     public Long runAsync(final ImportContext context, final ImportFilter filter) {
+        return runAsync(context, filter, false);
+    }
+
+    public Long runAsync(final ImportContext context, final ImportFilter filter, final boolean init) {
         return jobs.submit(new Job<ImportContext>() {
             @Override
             protected ImportContext call(ProgressMonitor monitor) throws Exception {
+                if (init) {
+                    init(context, true);
+                }
                 run(context, filter, monitor);
                 return context;
             }
